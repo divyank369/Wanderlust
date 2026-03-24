f82143a484e666bbd3abfeb4d1a88067592b554b
Divyank Singh <divyanksingh348@gmail.com>
2026-03-24 21:03:45 +0530

    chnging the hardcoded port

--- app.js
+++ app.js
@@ -146,6 +146,8 @@ app.use((err, req, res, next) => {
 //--------------------------------------
 // SERVER
 //--------------------------------------
-app.listen(8080, () => {
-  console.log("Server is listening to port 8080");
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
