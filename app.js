9376a8f3df5b64ce6fed1b1b63c9fe394408cb29
Divyank Singh <divyanksingh348@gmail.com>
2026-03-24 22:14:21 +0530

    Setup for Render deployment with dynamic PORT

--- app.js
+++ app.js
@@ -150,6 +150,7 @@ app.use((err, req, res, next) => {
 //--------------------------------------
 // SERVER
 //--------------------------------------
-app.listen(8080, () => {
-  console.log("Server is listening to port 8080");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
